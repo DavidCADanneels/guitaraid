a4f9cdf49f77938616646a0ea82a2a8201999aa1
David C.A. Danneels <daviddafke@msn.com>
2019-10-29 23:57:53 +0100

    add checkboxes for scaled notes

--- Application/src/main/groovy/FilterPanel.groovy
+++ Application/src/main/groovy/FilterPanel.groovy
@@ -3,11 +3,8 @@ import javax.swing.ButtonGroup
 import javax.swing.JCheckBox
 import javax.swing.JPanel
 import javax.swing.JRadioButton
-import javax.swing.border.BevelBorder
-import javax.swing.border.CompoundBorder
-import javax.swing.border.LineBorder
 import javax.swing.border.TitledBorder
-import java.awt.Color
+import java.awt.BorderLayout
 import java.awt.GridLayout
 import java.awt.event.ActionEvent
 import java.awt.event.ActionListener
@@ -16,12 +13,13 @@ import java.util.function.Predicate
 class FilterPanel extends JPanel implements ActionListener{
     GuitarStringsPanel guitarStringsPanel
     JRadioButton all, full, selection
-    HashMap<Note, JCheckBox> checkBoxes = new HashMap<>()
+    HashMap<Note, JCheckBox> notesCheckBoxes = new HashMap<>()
+    HashMap<ScaledNote, JCheckBox> scaledNoteCheckBoxes = new HashMap<>()
 
     FilterPanel(GuitarStringsPanel guitarStringsPanel) {
         this.guitarStringsPanel = guitarStringsPanel
 
-//        setLayout(new  BoxLayout(this, BoxLayout.Y_AXIS))
+        setLayout(new BorderLayout())
         JPanel optionsPanel = new JPanel()
 //        optionsPanel.setBorder(new CompoundBorder(new TitledBorder('Selection:'), new LineBorder(Color.BLACK)))
         optionsPanel.setBorder(new TitledBorder('Selection:'))
@@ -40,24 +38,52 @@ class FilterPanel extends JPanel implements ActionListener{
         full.addActionListener this
         selection.addActionListener this
         all.setSelected true
-        add optionsPanel
+        add optionsPanel, BorderLayout.WEST
 
-        JPanel boxes = new JPanel(new GridLayout(2,0))
-        boxes.setBorder(new TitledBorder('Displayed Notes:'))
+        JPanel selectedNotes = new JPanel(new GridLayout(2,0))
+        selectedNotes.setBorder(new TitledBorder('Displayed Notes:'))
         ['C','D','E','F','G','A','B','C#','D#', null,'F#','G#','A#',null].each { String name ->
             if(name == null){
-                boxes.add new JPanel()
+                selectedNotes.add new JPanel()
             } else {
                 Note note = Notes.notes.get(name)
                 JCheckBox checkBox = new JCheckBox(name)
-                checkBoxes.put(note, checkBox)
+                notesCheckBoxes.put(note, checkBox)
                 checkBox.selected = true
                 checkBox.enabled = false
                 checkBox.addActionListener this
-                boxes.add checkBox
+                selectedNotes.add checkBox
             }
         }
-        add boxes
+
+        JPanel selectedScaledNotes = new JPanel(new GridLayout(0, 6))
+        selectedScaledNotes.setBorder(new TitledBorder('Displayed Scaled Notes'))
+        [
+                null, null, null, null, 'E2', 'F2', 'F#2', 'G2', 'G#2', 'A2', 'A#2', 'B2',
+                'C3', 'C#3', 'D3', 'D#3', 'E3', 'F3', 'F#3', 'G3', 'G#3', 'A3', 'A#3', 'B3',
+                'C4', 'C#4', 'D4', 'D#4', 'E4', 'F4', 'F#4', 'G4', 'G#4', 'A4', 'A#4', 'B4',
+                'C5', 'C#5', 'D5', 'D#5', 'E5', 'F5', 'F#5', 'G5', 'G#5', 'A5', 'A#5', 'B5',
+                'C6', 'C#6', 'D6', null, null, null
+        ].each { String name ->
+            if(name == null){
+                selectedScaledNotes.add new JPanel()
+            } else {
+                ScaledNotes.scaledNotes.indexOf()
+                ScaledNote scaledNote = ScaledNotes.scaledNotesMap.get(name)
+                JCheckBox checkBox = new JCheckBox(name)
+                scaledNoteCheckBoxes.put(scaledNote, checkBox)
+                checkBox.selected = true
+                checkBox.enabled = false
+                checkBox.addActionListener this
+                selectedScaledNotes.add checkBox
+            }
+        }
+
+
+        JPanel center = new JPanel(new BorderLayout())
+        center.add selectedNotes, BorderLayout.NORTH
+        center.add selectedScaledNotes, BorderLayout.CENTER
+        add center, BorderLayout.CENTER
 
         setBorder(new TitledBorder('Filter'))
     }
@@ -78,25 +104,25 @@ class FilterPanel extends JPanel implements ActionListener{
 
     void actionPerformed(ActionEvent e) {
         if (e.source == all) {
-            checkBoxes.each { Note note, JCheckBox checkBox ->
+            notesCheckBoxes.each { Note note, JCheckBox checkBox ->
                 checkBox.enabled = false
                 checkBox.selected = true
             }
             guitarStringsPanel.filter = null
         } else if (e.source == full) {
-            checkBoxes.each { Note note, JCheckBox checkBox ->
+            notesCheckBoxes.each { Note note, JCheckBox checkBox ->
                 checkBox.enabled = false
                 checkBox.selected = fullScaledTone.test(note as Note)
             }
             guitarStringsPanel.filter = fullScaledTone
         } else if (e.source == selection) {
-            checkBoxes.each { Note note, JCheckBox checkBox ->
+            notesCheckBoxes.each { Note note, JCheckBox checkBox ->
                 checkBox.enabled = true
 //                checkBox.selected = true
             }
         } else {
             ArrayList<Note> notes = new ArrayList<>()
-            checkBoxes.each { Note note, JCheckBox checkBox ->
+            notesCheckBoxes.each { Note note, JCheckBox checkBox ->
                 if(checkBox.selected){
                     notes.add note
                 }
--- Elements/src/main/groovy/ScaledNotes.groovy
+++ Elements/src/main/groovy/ScaledNotes.groovy
@@ -1,28 +1,34 @@
 class ScaledNotes {
     static ArrayList<ScaledNote> scaledNotes = new ArrayList<>()
+    static HashMap<String, ScaledNote> scaledNotesMap = new HashMap<>()
     static {
         HashMap<String, Note> notes = Notes.notes
 
         // octave 2 incomplete
-        scaledNotes.add new ScaledNote(note: notes.get('E'), octave: 2)
-        scaledNotes.add new ScaledNote(note: notes.get('F'), octave: 2)
-        scaledNotes.add new ScaledNote(note: notes.get('F#'), octave: 2)
-        scaledNotes.add new ScaledNote(note: notes.get('G'), octave: 2)
-        scaledNotes.add new ScaledNote(note: notes.get('G#'), octave: 2)
-        scaledNotes.add new ScaledNote(note: notes.get('A'), octave: 2)
-        scaledNotes.add new ScaledNote(note: notes.get('A#'), octave: 2)
-        scaledNotes.add new ScaledNote(note: notes.get('B'), octave: 2)
+        addScaledNote new ScaledNote(note: notes.get('E'), octave: 2)
+        addScaledNote new ScaledNote(note: notes.get('F'), octave: 2)
+        addScaledNote new ScaledNote(note: notes.get('F#'), octave: 2)
+        addScaledNote new ScaledNote(note: notes.get('G'), octave: 2)
+        addScaledNote new ScaledNote(note: notes.get('G#'), octave: 2)
+        addScaledNote new ScaledNote(note: notes.get('A'), octave: 2)
+        addScaledNote new ScaledNote(note: notes.get('A#'), octave: 2)
+        addScaledNote new ScaledNote(note: notes.get('B'), octave: 2)
 
         // octave 3 to 5 complete
         [3,4,5].each { octave ->
             Notes.noteNames.each { note ->
-                scaledNotes.add new ScaledNote(note: notes.get(note), octave: octave)
+                addScaledNote new ScaledNote(note: notes.get(note), octave: octave)
             }
         }
 
         // octave 6 incomplete
-        scaledNotes.add new ScaledNote(note: notes.get('C'), octave: 6)
-        scaledNotes.add new ScaledNote(note: notes.get('C#'), octave: 6)
-        scaledNotes.add new ScaledNote(note: notes.get('D'), octave: 6)
+        addScaledNote new ScaledNote(note: notes.get('C'), octave: 6)
+        addScaledNote new ScaledNote(note: notes.get('C#'), octave: 6)
+        addScaledNote new ScaledNote(note: notes.get('D'), octave: 6)
+    }
+    
+    static addScaledNote(ScaledNote scaledNote){
+        scaledNotes.add(scaledNote)
+        scaledNotesMap.put(scaledNote.name, scaledNote)
     }
 }
