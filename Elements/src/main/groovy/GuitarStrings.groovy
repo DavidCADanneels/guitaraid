3f6282ac8296968a8175b51940cff21868e8b40a
David C.A. Danneels <daviddafke@msn.com>
2019-10-29 13:01:15 +0100

    add guitarStrings + tests

--- Elements/src/main/groovy/GuitarStrings.groovy
+++ Elements/src/main/groovy/GuitarStrings.groovy
@@ -3,18 +3,17 @@ class GuitarStrings {
 
     GuitarStrings() {
         HashMap<String, Note> notes = Notes.notes
-        def lowE = new GuitarString(name: 'E-low',
-                notes: [
-                        new ScaledNote(note: notes.get('E'), octave: 2),
-                        new ScaledNote(note: notes.get('F'), octave: 2),
-                        new ScaledNote(note: notes.get('F#'), octave: 2),
-                        new ScaledNote(note: notes.get('G'), octave: 2),
-                        new ScaledNote(note: notes.get('G#'), octave: 2),
-                        new ScaledNote(note: notes.get('A'), octave: 2),
-                        new ScaledNote(note: notes.get('A#'), octave: 2),
-                        new ScaledNote(note: notes.get('B'), octave: 2),
-                        new ScaledNote(note: notes.get('C'), octave: 3),
-                ])
+        def lowE = new GuitarString(name: 'E-low', notes: ScaledNotes.scaledNotes.subList(0, 23))
+        def A = new GuitarString(name: 'A', notes: ScaledNotes.scaledNotes.subList(5, 28))
+        def D = new GuitarString(name: 'D', notes: ScaledNotes.scaledNotes.subList(10, 33))
+        def G = new GuitarString(name: 'G', notes: ScaledNotes.scaledNotes.subList(15, 38))
+        def B = new GuitarString(name: 'B', notes: ScaledNotes.scaledNotes.subList(19, 42))
+        def highE = new GuitarString(name: 'E-high', notes: ScaledNotes.scaledNotes.subList(24, 47))
         guitarStrings.put('E-low', lowE)
+        guitarStrings.put('A', A)
+        guitarStrings.put('D', D)
+        guitarStrings.put('G', G)
+        guitarStrings.put('B', B)
+        guitarStrings.put('E-high', highE)
     }
 }
--- Elements/src/test/groovy/GuitarStringsTest.groovy
+++ Elements/src/test/groovy/GuitarStringsTest.groovy
@@ -3,13 +3,51 @@ import org.junit.Test
 class GuitarStringsTest {
 
     @Test
-    void 'test constructor'(){
+    void 'test low E string'(){
+        testString('E-low', 'E', 2, 'D', 4)
+    }
+
+    @Test
+    void 'test A string'(){
+        testString('A', 'A', 2, 'G', 4)
+    }
+
+    @Test
+    void 'test D string'(){
+        testString('D', 'D', 3, 'C', 5)
+    }
+
+    @Test
+    void 'test G string'(){
+        testString('G', 'G', 3, 'F', 5)
+    }
+
+    @Test
+    void 'test B string'(){
+        testString('B', 'B', 3, 'A', 5)
+    }
+
+    @Test
+    void 'test high E string'(){
+        testString('E-high', 'E', 4, 'D', 6)
+    }
+
+    void testString(String name, String startNote, Integer startOctave, String endNote, Integer endOctave){
         def strings = new GuitarStrings()
-        GuitarString lowE = strings.guitarStrings.get('E-low')
-        ScaledNote firstScaledNote = lowE.notes.get(0)
-        assert firstScaledNote.name == 'E2'
-        assert firstScaledNote.octave == 2
-        Note note = firstScaledNote.note
-        assert note.name == 'E'
+        GuitarString guitarString = strings.guitarStrings.get(name)
+        List<ScaledNote> notes = guitarString.notes
+        assert notes.size() == 23
+
+        ScaledNote firstScaledNote = notes.get(0)
+        assert firstScaledNote.name == "${startNote}${startOctave}"
+        assert firstScaledNote.octave == startOctave
+        Note firstNote = firstScaledNote.note
+        assert firstNote.name == startNote
+
+        ScaledNote lastScaledNote = notes.get(22)
+        assert lastScaledNote.name == "${endNote}${endOctave}"
+        assert lastScaledNote.octave == endOctave
+        Note lastNote = lastScaledNote.note
+        assert lastNote.name == endNote
     }
 }
